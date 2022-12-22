59b2b5e03a090746453de90be35ad0d5895b8d81
syahrandywaskito <syahrandywaskito666@gmail.com>
2022-12-22 22:25:03 +0700

    add header file

--- src/basic.cpp
+++ src/basic.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
+#include "includes/header.h"
 
 // macro directive
 #define MY_AGE 18
 
 int main(){
+	hallo();
 	std::cout << "basic cpp\n";
 	std::cout << "My Age is : " << MY_AGE << std::endl;
 	return 0;
+}
+
+void hallo(){
+	std::cout << "hallo\n";
 }
