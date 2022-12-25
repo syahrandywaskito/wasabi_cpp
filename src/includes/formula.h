c08742554b41a4a6a7d4905359c9971248d8b3f2
syahrandywaskito <syahrandywaskito666@gmail.com>
2022-12-25 21:32:15 +0700

    commit 25-12-2022 21.32

--- src/includes/formula.h
+++ src/includes/formula.h
@@ -4,6 +4,11 @@
  * Rumus akan selalu diupdate berkala
 */
 
+#ifndef FORMULA_H
+#define FORMULA_H
+
 int areaTriangle(int base, int height);
 int aroundTriangle(int side1, int side2, int side3);
 int areaSquare(int side);
+
+#endif
--- src/includes/header.h
+++ src/includes/header.h
@@ -1,1 +1,6 @@
+#ifndef HEADER_H
+#define HEADER_H
+
 void hallo();
+
+#endif
