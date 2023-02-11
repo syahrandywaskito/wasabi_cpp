577b7f538282fdb51c542a8fecea0fb8371458e0
syahrandywaskito <syahrandywaskito666@gmail.com>
2023-02-11 21:55:59 +0700

    debug lesson

--- src/lingkaran.cpp
+++ src/lingkaran.cpp
@@ -6,24 +6,23 @@
 */
 #include <iostream>
 #include <math.h>
-using namespace std;
 
 // PI digunakan sebagai alat hitungan untuk jari-jari tidak habis dibagi 7
-#define PI 3.14
+const float PI{3.14};
 
 int main(int argc, char const *argv[]) {
-  int radius = 0, diameter = 0, varContainer, modulo;
-  float result = 0;
+  int radius{0}, diameter{0}, temp{0}, modulo{0};
+  float result{0};
   //input
-  cout << "masukkan jari-jari = "; cin >> radius;
+  std::cout << "masukkan jari-jari = "; std::cin >> radius;
 
   // process
   // find diameter
   diameter = radius * 2;
-  cout << "diameter = " << diameter << endl;
+  std::cout << "diameter = " << diameter << "\n";
   //assign nilai untuk proses pengambilan nilai sisa bagi
-  varContainer = radius;
-  modulo = varContainer % 7;
+  temp = radius;
+  modulo = temp % 7;
   // pengecekan apakah jari-jari habis dibagi 7 atau tidak
   if (modulo == 0) {
     result = 22 * pow(radius, 2) / 7;
@@ -33,6 +32,6 @@ int main(int argc, char const *argv[]) {
   }
   
   // output
-  cout << "luas lingkaran = " << result << endl;
+  std::cout << "luas lingkaran = " << result << std::endl;
   return 0;
 }
